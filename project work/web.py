cdf2c3ea83b632c12eb670027ca8471a8e2276d3
Anjali <anjali9140747848@gmail.com>
2023-10-11 08:06:35 +0530

    stringprac6

---
+++ project work/web.py
@@ -0,0 +1,8 @@
+from constants import  MAIN_CATEGORY,COMMUNITY,SERVECIES,HOUSING, FOR_SALE,JOBS , GIGS
+import time
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+import sys ,os
+from pyQt5.Qtwidgets import 
