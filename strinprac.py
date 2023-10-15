842d25f2f5b9db93035d83abc58fe3d6ca5b23d1
Anjali <anjali9140747848@gmail.com>
2023-10-15 12:03:56 +0530

    string2.0

---
+++ strinprac.py
@@ -0,0 +1,12 @@
+str="may i go to washroom"
+str1='''many many day .../
+a man have to live in village '''
+print(str)
+print(str1)
+
+print(str+str1)
+print(str * 2)
+msg="mmmmmaaaabbbbbyyyyyy"
+print(len(msg))
+print(max(msg))
+print(min(msg))
